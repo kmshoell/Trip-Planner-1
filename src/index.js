45a0b2503ba7798bacfbcc0cf26ea1788843732f
kmshoell <34169711+kmshoell@users.noreply.github.com>
2020-10-05 13:20:37 -0400

    map not function: can connect to mapblock

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 import mapboxgl from "mapbox-gl";
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia21zaG9lbGwiLCJhIjoiY2tmd3E1enRrMGxqdjJ3azB6Z2h2aWQwciJ9.VF-xwJMrBLCTvOH647DTnQ';
+mapboxgl.accessToken = 'pk.eyJ1Ijoia21zaG9lbGwiLCJhIjoiY2tmd3NqZHljMTY5cjJ4bXRnOGVtbDkzayJ9.q3ntiPlClNLtB2tMRc9nOA';
 let map = new mapboxgl.Map({
   container: 'map',
   center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
